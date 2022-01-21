c0be96e331e933e4776567c855de8f01118b489b
Nabil Fawzy <nabilfawzy507@gmail.com>
2022-01-22 01:06:03 +0200

    update angular setup 7

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { IPagination } from './Models/pagination';
-import { IProduct } from './Models/product';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  title = 'Shopping';
-  products: IProduct[];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    } , error => {
-      console.log(error);
-    });
-
-  }
-
+export class AppComponent {
+  title = 'client';
 }
