c113d0f7e050da8ac87b93d5fa7f5400f7d7922d
Nabil Fawzy <nabilfawzy507@gmail.com>
2022-01-23 22:57:26 +0200

    UI angular functionality search filter pagination 9

--- client/src/app/models/product.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct {
-        id: number;
-        name: string;
-        description: string;
-        price: number;
-        picturseUrl: string;
-        productType: string;
-        productBrand: string;
-    }
